a59d563fc9a10850beb59c01feaf926eae6b380d
Neyko Naydenov <shendamere@gmail.com>
2021-09-15 13:10:43 +0300

    Add files via upload
    Front-end

---
+++ MusicQuizz-frontend/mq-frontend/src/App.js
@@ -0,0 +1,14 @@
+//import React, { useState } from "react";
+import playButton from "./Components/PlayButton";
+import './App.css';
+
+function App() {
+  //const []
+  return (
+    <div className="App">
+    <playButton/>
+    </div>
+  );
+}
+
+export default App;
---
+++ MusicQuizz-frontend/mq-frontend/src/Components/PlayButton.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const playButton = ({onClick}) => {
+    return (
+    <button>Hey!</button>
+    );
+}
+export default playButton;
